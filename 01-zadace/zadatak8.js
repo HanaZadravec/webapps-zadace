a1a28bf887e9f67d7158c8f0d7d7ff560b3bd725
HanaZadravec <zadravechana@gmail.com>
2022-10-07 17:41:45 +0200

    Rjesen zadatak 8

---
+++ 01-zadace/zadatak8.js
@@ -0,0 +1,7 @@
+/*Napiši funkciju koja vraća predano polje obrnutim redosljedom*/
+
+const obrnuto = (polje) => {
+  return polje.reverse(); // postoji metoda reverse u jsu koja nam omogucuje da ovaj zadatak rijesimo na vrlo brz nacin
+};
+
+console.log(obrnuto([3, 4, 5, 6]));
